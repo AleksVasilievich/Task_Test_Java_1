4c115c1c2aeafcfc67932fa5b68d9b7f705796be
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-12-06 00:13:52 +0300

    add

---
+++ program.java
@@ -0,0 +1,15 @@
+/**
+ * program
+ */
+public class program {
+
+    public static void main(String[] args) {
+        {
+            System.out.println("world");
+        }
+        System.out.println("world");
+        {
+            
+        }
+    }
+}
