250b9167cf274bddfed890075112b3ca20f77b02
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-12-09 00:53:37 +0300

    add

--- program_3.java
+++ program_3.java
@@ -1,22 +1,43 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class program_3 {
-    public static void main(String[] args) { 
-        int size = getValue("Введите размер");
-        int[] bar = create(size);
-        fill(bar);
-        print(bar);
+    public static void main(String[] args) {
+        int count = getValue("Введите размер");
+        int[] array = create(count);
+        fill(array);
+        print(array);
 
     }
-        static int[] create(int count) {
-            return null;
-        }
 
-        static void print(int[] array) {
-        }
+    static int getValue(String text) {
+        System.out.println("Введите размер массива");
 
-        static void fill(int[] array) {    
-        }
+        try (Scanner scanner = new Scanner(System.in)) {
 
-        static int getValue(String text) {
-            return -1;
+            int count = scanner.nextInt();
+            return count;
         }
     }
+
+    static int[] create(int count) {
+        int[] array = new int[count];
+        return array;
+    }
+
+    static void fill(int[] array) {
+        Random r = new Random();
+        for (int i = 0; i < array.length; i++)
+            array[i] = r.nextInt(10);
+
+    }
+
+    static void print(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%d ", array[i]);
+        }
+        // Scanner scanner;
+        // scanner.close();
+    }
+
+}
