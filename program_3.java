0f3e7568a569a956b5bcfdce8b56258b60816275
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-12-09 23:08:52 +0300

    add

--- program_3.java
+++ program_3.java
@@ -2,12 +2,15 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class program_3 {
+    // private static int i;
+
     public static void main(String[] args) {
         int count = getValue("Введите размер");
         int[] array = create(count);
         fill(array);
+        // print(array);
+        meetingFrequency(array);
         print(array);
-
     }
 
     static int getValue(String text) {
@@ -29,7 +32,21 @@ public class program_3 {
         Random r = new Random();
         for (int i = 0; i < array.length; i++)
             array[i] = r.nextInt(10);
+    }
 
+    static void meetingFrequency(int[] array) {
+        // int elem;
+        // elem = 0;
+        int num;
+        num = 0;
+        for (int i = 0; i < array.length; i++)
+            for (int j = 0; j < array.length; j++)
+                // elem = array[i];
+                if (array[i] == array[j]) {
+                    num++;
+        System.out.printf("i=%d; num=%d =>  ", array[i], num);
+        num = 0;
+        }
     }
 
     static void print(int[] array) {
