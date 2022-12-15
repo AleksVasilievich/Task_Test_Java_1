037bbbbbc87d203a10f40ad98f3e145ebee2a5fa
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2022-12-15 22:52:56 +0300

    add

--- testJava/test_8.java
+++ testJava/test_8.java
@@ -3,16 +3,16 @@ package testJava;
 public class test_8 {
     public static void main(String[] args) {
         int towers = 3;
-        go(towers, 'A',  'B', 'C');
+        go(towers, 'A', 'B', 'C');
     }
-    
+
     public static void go(int up, char from, char transit, char to) {
         if (up == 1)
-            System.out.println("Disk 1 from " + from + " to " + to);
+            System.out.println("Диск 1 из " + from + " к " + to);
         else {
             go(up - 1, from, to, transit);
-            System.out.println("Disk "
-                    + up + " from " + from + " to " + to);
+            System.out.println("Диск "
+                    + up + " из " + from + " к " + to);
             go(up - 1, transit, from, to);
         }
     }
